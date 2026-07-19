eea4ccedf840adee4086477221dd043bd3fe3cd4
joshremender <remender@purdue.edu>
2026-07-19 13:01:09 -0400

    Create revert.sh

---
+++ hooks/revert.sh
@@ -0,0 +1,29 @@
+set -x
+
+#reboot
+
+source "/etc/libvirt/hooks/kvm.conf"
+
+modprobe -r vfio
+modprobe -r vfio_pci
+modprobe -r vfio_iommu_type1
+
+sleep 10
+
+virsh nodedev-reattach $VIRSH_GPU_VIDEO
+virsh nodedev-reattach $VIRSH_GPU_AUDIO
+
+echo 1 > /sys/class/vtconsole/vtcon0/bind
+echo 1 > /sys/class/vtconsole/vtcon1/bind
+
+sleep 3
+
+echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+modprobe amdgpu
+modprobe snd_hda_intel
+
+sleep 3
+
+systemctl start display-manager
+systemctl start sddm.service
